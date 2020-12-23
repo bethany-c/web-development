1be6d747e442dbb7f62add898935bc59d829a0a0
Bethany C <bethanylyc@gmail.com>
2020-12-22 22:58:07 -0700

    spacing

--- mod4-solution/script.js
+++ mod4-solution/script.js
@@ -26,15 +26,15 @@ WARNING!!! WARNING!!!
 */
 
 (function (window) {
-var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
+  var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
 
-for (var i=0; i<names.length; i++) {
-    var firstLetter = names[i].charAt(0).toLowerCase();
-    if (firstLetter == 'j') {
-      byeSpeaker(names[i]);
-    } 
-    else {
-      helloSpeaker(names[i]);
-    }
-}
+  for (var i=0; i<names.length; i++) {
+      var firstLetter = names[i].charAt(0).toLowerCase();
+      if (firstLetter == 'j') {
+        byeSpeaker(names[i]);
+      } 
+      else {
+        helloSpeaker(names[i]);
+      }
+  }
 })(window);
